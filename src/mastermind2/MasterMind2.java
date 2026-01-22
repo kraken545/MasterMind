63af0e55516e1c13542dda57e68e9e06d9dcc988
Elian Meran Rosario <1217378@student.roc-nijmegen.nl>
2026-01-22 09:40:32 +0100

    areglado

--- src/mastermind2/MasterMind2.java
+++ src/mastermind2/MasterMind2.java
@@ -14,7 +14,7 @@ public class MasterMind2
 											 * @author Elian Calling methods in the main class
 											 * 
 											 **/
-		Scanner input = new Scanner(System.in); 
+		Scanner input = new Scanner(System.in);
 		/**
 		 * @author Elian Importing Scanner ;) .
 		 **/
@@ -28,18 +28,28 @@ public class MasterMind2
 						 * @author Elian if true games keep going. if false, means user have fill the
 						 *         correct answer and the program Congratulate the player.
 						 **/
-		int trys; /**
-					 * @author Elian variable to hold the amount of attempts the user have.
-					 **/
+		int trys = 0; /**
+						 * @author Elian variable to hold the amount of attempts the user have.
+						 **/
 		int attempts; /**
-		 * @author Elian variable to hold the amount of attempts the user used.
+						 * @author Elian variable to hold the amount of attempts the user used.
+						 **/
+		methods.show_dificulty();
+		/**
+		 * @author Elian to show the table of difficulty's to the user.
 		 **/
-		methods.show_dificulty();/**
-									 * @author Elian to show the table of difficulty's to the user.
-									 **/
+		
+			try
+			{
 
-		trys = methods.dificulty(input.nextInt());
+				trys = methods.dificulty(input.nextInt());
 
+			} catch (Exception e)
+			{
+				System.out.println("Verkeerde invoer");
+				trys = 8;
+			}
+		
 		correctColors = new Colors[4];
 		for (int i = 0; i < 4; i++)
 		{
@@ -51,10 +61,12 @@ public class MasterMind2
 		while (attempts < trys)
 		{
 			playerGuesses = new String[4];
+			methods.spatie();
 			methods.mogelijke_kleuren();
+			methods.spatie();
 			for (int i = 0; i < 4; i++)
 			{
-				
+
 				methods.spatie();
 				methods.Vak(i);
 				playerGuesses[i] = input.next();
